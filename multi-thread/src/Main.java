f0f2b2a61c31ce8f046995e07dc96df818b51f51
병우 유 <conner903@naver.com>
2021-02-19 20:34:42 +0900

    Study ThreadPool

--- multi-thread/src/Main.java
+++ multi-thread/src/Main.java
@@ -2,50 +2,13 @@ import Study.BasicTest;
 import Study.ControllThreadState.InterruptMethod.InterruptExample;
 import Study.ThreadGroup.ThreadGroupExample;
 import Study.ThreadGroup.ThreadinfoExample;
+import Study.ThreadPool.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        //Study.BeepPrintExample.BeepPrintExample1 beepPrintExample1 = new Study.BeepPrintExample.BeepPrintExample1();
-        //beepPrintExample1.beepPrint();
 
-        //Study.BeepPrintExample.BeepPrintExample2 beepPrintExample2 = new Study.BeepPrintExample.BeepPrintExample2();
-        //beepPrintExample2.beepPrint();
-
-        //BeepPrintExample3 beepPrintExample3 = new BeepPrintExample3();
-        //beepPrintExample3.beepPrint();
-
-        //ThreadNameExample threadNameExample = new ThreadNameExample();
-        //threadNameExample.threadNamePrint();
-
-        //PriorityExample priorityExample = new PriorityExample();
-        //priorityExample.printPriorityThread();
-
-        //MainThreadExample mainThreadExample = new MainThreadExample();
-        //mainThreadExample.mainThreadRun();
-
-        //ThreadStateExample threadStateExample = new ThreadStateExample();
-        //threadStateExample.threadStatePrint();
-
-        //SleepExample sleepExample = new SleepExample();
-        //sleepExample.sleepExampleRun();
-
-        //YieldExample yieldExample = new YieldExample();
-        //yieldExample.YieldExampleRun();
-
-        //JoinExample joinExample = new JoinExample();
-        //joinExample.joinExampleRun();
-
-        //WaitNotifyExample waitNotifyExample = new WaitNotifyExample();
-        //waitNotifyExample.waitNotifyExampleRun2();
-
-        //StopFlagExample stopFlagExample = new StopFlagExample();
-        //stopFlagExample.stopFlagExampleRun();
-
-        //InterruptExample interruptExample = new InterruptExample();
-        //interruptExample.run();
-
-        BasicTest basicTest = new ThreadGroupExample();
+        BasicTest basicTest = new CallbackExample();
         basicTest.run();
 
     }
--- multi-thread/src/Study/BasicTest.java
+++ multi-thread/src/Study/BasicTest.java
@@ -3,4 +3,6 @@ package Study;
 public class BasicTest {
 
     public void run(){};
+
+    public void runWithException() throws InterruptedException {};
 }
