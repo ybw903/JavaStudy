ec4bf9606a4473b22e9963c6cdc313ca0f9497ad
ybw903 <conner903@naver.com>
2021-12-18 15:09:29 +0900

    feat choose method of menu

--- coffee-order/src/main/java/domain/menu/Menu.java
+++ coffee-order/src/main/java/domain/menu/Menu.java
@@ -4,8 +4,15 @@ import java.util.List;
 
 public class Menu implements IMenu{
 
+    private List<MenuItem> items;
+
     @Override
     public MenuItem choose(String name) {
+        for (MenuItem item : items) {
+            if (item.getName().equals(name)) {
+                return item;
+            }
+        }
         return null;
     }
 }
