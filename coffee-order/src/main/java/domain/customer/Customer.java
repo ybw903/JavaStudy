0b502c85c01880f1b233cc41da069e5ea006c387
ybw903 <conner903@naver.com>
2021-12-18 15:07:52 +0900

    feat order method of customer

--- coffee-order/src/main/java/domain/customer/Customer.java
+++ coffee-order/src/main/java/domain/customer/Customer.java
@@ -1,11 +1,14 @@
 package domain.customer;
 
 import domain.barista.Barista;
+import domain.coffee.Coffee;
 import domain.menu.Menu;
+import domain.menu.MenuItem;
 
 public class Customer implements ICustomer{
     @Override
     public void order(String menuName, Menu menu, Barista barista) {
-
+        MenuItem menuItem = menu.choose(menuName);
+        Coffee coffee = barista.makeCoffee(menuItem);
     }
 }
